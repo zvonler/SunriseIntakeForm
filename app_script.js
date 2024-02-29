7f0731722ee84374bbe41a3f48ca3d611383305c
Zachary Vonler <zvonler@gmail.com>
2024-02-29 16:13:24 -0600

    Changed to OOP style and added some features

--- app_script.js
+++ app_script.js
@@ -1,110 +1,145 @@
-
-// The sheetID value is part of the URL of the Sheets document
-var sheetID = "1xkdGSftOA6P1OyQ9MW9hJ1MdvUk1LSKjxBNaklRnIhY";
-// The formID value is part of the URL of the Forms document
-var formID = "1cwhEGVsxbwHkM1ZMZq_zW9SZrkjDJmnb7X7XIgLRcdc";
-
-var spreadSheet = SpreadsheetApp.openById(sheetID);
-var form = FormApp.openById(formID);
-
-var pageBreaksByName = new Map();
-var optionsByDropdown = new Map();
-var sectionDestinations = new Map();
-
-pageBreaksByName.set("SUBMIT FORM", FormApp.PageNavigationType.SUBMIT);
-
 function main() {
-  clearForm();
-  createSections();
-  addDropdownChoices();
-  setSectionDestinations();
-}
-
-function setSectionDestinations() {
-  for (const [source, dest] of sectionDestinations) {
-    source.setGoToPage(pageBreaksByName.get(dest));
+  var spreadSheet = SpreadsheetApp.getActiveSpreadsheet();
+  var firstSheetName = spreadSheet.getSheets()[0].getName();
+  if (firstSheetName != "Start Page") {
+    throw "Error: containing Sheets document doesn't have expected layout"
   }
+  var form = getForm(spreadSheet);
+  (new FormBuilder(form)).buildFrom(spreadSheet);
 }
 
-function addDropdownChoices() {
-  for (const [dropdown, options] of optionsByDropdown) {
-    var choices = [];
-    for (o of options) {
-      if (o[1] == "") {
-        var choice = dropdown.createChoice(o[0], FormApp.PageNavigationType.CONTINUE);
-        choices.push(choice);
-      } else {
-        var choice = dropdown.createChoice(o[0], pageBreaksByName.get(o[1]));
-        choices.push(choice);
-      }
-    }
-    dropdown.setChoices(choices);
-  };
+// Returns the target Form ID from the spreadsheet
+function getForm(spreadSheet) {
+  // The Sheets document must have a Named Range named "FORMID" that
+  // contains a single cell with the ID of the target Forms document.
+  var formIDRange = spreadSheet.getRangeByName("FORMID");
+  var formID = formIDRange.getValue();
+  if (!formID.match(/^[0-9a-zA-Z_]+$/)) {
+    throw "Didn't get valid form ID string. Does sheet have a range named 'FORMID'?";
+  }
+  return FormApp.openById(formID);
 }
 
-function createSections() {
-  var sheet_i = 0;
-  var sectionDestination = null;
-  spreadSheet.getSheets().forEach(function (sheet) {
-    var sectionName = sheet.getName();
-    if (sheet_i > 0) {
-      var section = form.addPageBreakItem();
-      section.setTitle(sectionName);
-      if (sectionDestination) {
-        sectionDestinations.set(section, sectionDestination);
-        sectionDestination = null;
-      }
-      pageBreaksByName.set(sectionName, section);
-    }
-    sheet_i++;
+class FormBuilder {
+  // Creates a new instance that will build the target
+  constructor(targetForm) {
+    this._targetForm = targetForm;
+  }
 
-    var data = sheet.getDataRange().getValues();
-    var question = "";
-    var options = [];
-    data.forEach(function (row) {
-      if (row[0]) { // New question or goto
-        if (row[0].startsWith("GOTO ")) {
-          sectionDestination = row[0].substring(5);
-        } else {
-          question = row[0];
+  // Builds the target form using the contents of spreadSheet
+  buildFrom(spreadSheet) {
+    this._initMembers();
+    this._clearForm();
+    this._createSections(spreadSheet);
+    this._addDropdownChoices();
+    this._setSectionDestinations();
+  }
+
+  _initMembers() {
+    this._pageBreaksByName = new Map();
+    this._optionsByDropdown = new Map();
+    this._sectionDestinations = new Map();
+
+    // Adding this entry means that a GOTO row in the sheet can use SUBMIT FORM
+    // as if it were a regular section name.
+    this._pageBreaksByName.set("SUBMIT FORM", FormApp.PageNavigationType.SUBMIT);
+  }
+
+  _clearForm() {
+    // Removes all existing form sections and questions
+    var items = this._targetForm.getItems();
+    var count = items.length;
+    for (var i = 0; i < count; ++i) {
+      this._targetForm.deleteItem(0);
+    }
+  }
+
+  _createSections(spreadSheet) {
+    var sheet_i = 0;
+    var sectionDestination = null;
+    var self = this;
+    spreadSheet.getSheets().forEach(function (sheet) {
+      var sectionName = sheet.getName();
+      var section;
+      if (sheet_i > 0) {
+        section = self._targetForm.addPageBreakItem();
+        section.setTitle(sectionName);
+        if (sectionDestination) {
+          self._sectionDestinations.set(section, sectionDestination);
+          sectionDestination = null;
         }
-      } else if (row[1]) { // New option
-        options.push([row[1], row[2]]);
-      } else if (question != "") { // End of options
-        createQuestion(form, question, options);
-        question = "";
-        options.length = 0;
+        self._pageBreaksByName.set(sectionName, section);
+      }
+      sheet_i++;
+
+      var data = sheet.getDataRange().getValues();
+      var question = "";
+      var options = [];
+      data.forEach(function (row) {
+        if (row[0]) {
+          if (row[0].match(/^[ \t]*\/\//)) {
+            // Ignore the row
+          } else if (row[0] == "TITLE") {
+            section.setTitle(row[1]);
+          } else if (row[0] == "DESCRIPTION") {
+            section.setHelpText(row[1]);
+          } else if (row[0] == "QUESTION") {
+            question = row[1];
+          } else if (row[0] == "CHOICE") {
+            options.push([row[1], row[3]]);
+          } else if (row[0] == "GOTO") {
+            sectionDestination = row[1];
+          }
+        } else if (question != "") { // End of options
+          self._createQuestion(question, options);
+          question = "";
+          options.length = 0;
+        }
+      });
+
+      // Check if the last question wasn't added to the form
+      if (question != "") {
+        self._createQuestion(question, options);
       }
     });
+  }
 
-    // Check if the last question wasn't added to the form
-    if (question != "") {
-      createQuestion(form, question, options);
+  _createQuestion(question, options) {
+    if (options.length > 1) {
+      this._createDropdown(question, options);
+    } else if (options.length == 1) {
+      this._targetForm.addDateItem().setTitle(question);
+    } else {
+      this._targetForm.addTextItem().setTitle(question);
     }
-  });
-}
+  }
 
-function createQuestion(form, question, options) {
-  if (options.length > 0) {
-    createDropdown(form, question, options);
-  } else {
-    form.addTextItem().setTitle(question);
+  _createDropdown(question, options) {
+    var dropdown = this._targetForm.addListItem();
+    dropdown.setTitle(question);
+    dropdown.setRequired(true);
+    this._optionsByDropdown.set(dropdown, options.slice());
+  }
+
+  _addDropdownChoices() {
+    for (const [dropdown, options] of this._optionsByDropdown) {
+      var choices = [];
+      for (let o of options) {
+        if (o[1] == "") {
+          var choice = dropdown.createChoice(o[0], FormApp.PageNavigationType.CONTINUE);
+          choices.push(choice);
+        } else {
+          var choice = dropdown.createChoice(o[0], this._pageBreaksByName.get(o[1]));
+          choices.push(choice);
+        }
+      }
+      dropdown.setChoices(choices);
+    };
+  }
+
+  _setSectionDestinations() {
+    for (const [source, dest] of this._sectionDestinations) {
+      source.setGoToPage(this._pageBreaksByName.get(dest));
+    }
   }
 }
-
-function createDropdown(form, question, options) {
-  var dropdown = form.addListItem();
-  dropdown.setTitle(question);
-  dropdown.setRequired(true);
-  optionsByDropdown.set(dropdown, options.slice());
-}
-
-// Removes all existing form sections and questions
-function clearForm() {
-  var items = form.getItems();
-  var count = items.length;
-  for (var i = 0; i < count; ++i) {
-    form.deleteItem(0);
-  }
-}
-
